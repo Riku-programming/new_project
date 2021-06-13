ac2c4ee92a3573cf12779e56084f965dc1fb8fdf
Riku-programming <riku_programming@gmail.com>
2021-06-13 16:11:58 +0900

    template.htmlとtmp.htmlをコピーする機能を別の関数に切り分け

--- readHtml/readHtml.go
+++ readHtml/readHtml.go
@@ -14,8 +14,6 @@ import (
 //Matchで変換したいファイルを指定してる
 // todo 将来的にこれを内包した関数にしたい
 
-// todo 元のファイルをコピーする関数を作っておきたい(現在だと元のファイルが消えちゃう）
-
 func Replace(path string, fi os.FileInfo, err error) error {
 	if err != nil {
 		return err
@@ -37,14 +35,7 @@ func Replace(path string, fi os.FileInfo, err error) error {
 		replacer := strings.NewReplacer("Hello", "Goodbye", "world", "5000", "date", TimeToString.TimeToString("01/02", time.Now()))
 		newContents := replacer.Replace(string(read))
 		err = ioutil.WriteFile(path, []byte(newContents), 0)
-		//fixme 絶対パスを変更する Rename関数を作ってそこに入れる
-		if err := os.Rename("/Users/riku/go/src/pdf_new_app/template/template.html", "/Users/riku/go/src/pdf_new_app/template/result.html"); err != nil {
-			fmt.Println(err)
-		}
-		if err := os.Rename("/Users/riku/go/src/pdf_new_app/template/tmp.html", "/Users/riku/go/src/pdf_new_app/template/template.html"); err != nil {
-			fmt.Println(err)
-		}
-
+		RenameTmpAndTemplate()
 		if err != nil {
 			panic(err)
 		}
@@ -53,14 +44,22 @@ func Replace(path string, fi os.FileInfo, err error) error {
 }
 
 func CopyFile() {
+	//fixme 絶対パスを変更する
 	b, err := ioutil.ReadFile("/Users/riku/go/src/pdf_new_app/template/template.html")
 	if err != nil {
 		panic(err)
 	}
-
-	// write the whole body at once
 	err = ioutil.WriteFile("/Users/riku/go/src/pdf_new_app/template/tmp.html", b, 0644)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func RenameTmpAndTemplate() {
+	if err := os.Rename("/Users/riku/go/src/pdf_new_app/template/template.html", "/Users/riku/go/src/pdf_new_app/template/result.html"); err != nil {
+		fmt.Println(err)
+	}
+	if err := os.Rename("/Users/riku/go/src/pdf_new_app/template/tmp.html", "/Users/riku/go/src/pdf_new_app/template/template.html"); err != nil {
+		fmt.Println(err)
 	}
 }
